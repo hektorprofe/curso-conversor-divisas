e21c04df632bed65632519601ef8f28924c7f136
Hektor <hcostaguzman@gmail.com>
2019-04-20 18:18:50 +0200

    conversor listo

--- scripts.js
+++ scripts.js
@@ -1,12 +1,25 @@
 let inputs = document.querySelectorAll(".valor");
 
-inputs.forEach((input) => {
-  input.value = input.dataset.cambio;
-});
+let url = 'https://api.exchangeratesapi.io/latest?symbols=USD,GBP,JPY';
+fetch(url)
+  .then(r => r.json())
+  .then(data => {
+    document.querySelector('#USD')
+      .dataset.cambio = data.rates.USD;
+    document.querySelector('#GBP')
+      .dataset.cambio = data.rates.GBP;  
+    document.querySelector('#JPY')
+      .dataset.cambio = data.rates.JPY;
+
+    inputs.forEach(input => {
+      input.value = input.dataset.cambio;
+    });
+  })
+  .catch(error => console.error(error))
 
 function valorCambiado(input) {
   let factor = input.value / input.dataset.cambio;
-  inputs.forEach((campo) => {
+  inputs.forEach(campo => {
     campo.value = (campo.dataset.cambio * factor).toFixed(2);
   })
 }
